f5e3e91fa9189544d291753913381600244437d6
kongoloa <aristoteshako@gmail.com>
2022-03-24 14:03:48 -0700

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """
-Script generates a .tgz archive from web_static folder
+A Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy
 """
 from fabric.operations import local, run, put, env
 from datetime import datetime
 import os
 
-
 env.hosts = ['35.237.142.70', '35.231.108.250']
 env.user = 'ubuntu'
 
-
 def do_pack():
-    """
-    function creates a .tgz archive
-    """
-
+    """ Function to compress files in an archive """
     name = "versions/web_static_{}.tgz"
     name = name.format(datetime.now().strftime("%Y%m%d%H%M%S"))
     local("mkdir -p versions")
@@ -25,11 +20,8 @@ def do_pack():
     else:
         return None
 
-
 def do_deploy(archive_path):
-    """
-    function to dist to web server
-    """
+    """ function to dist to web server """
     if not os.path.exists(archive_path):
         return False
     if not put(archive_path, "/tmp/").succeeded:
@@ -55,9 +47,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """
-    function to dep
-    """
+    """ function to deploy """
     dep = do_pack()
     if dep is False:
         return False
